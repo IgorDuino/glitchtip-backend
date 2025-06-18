a2493fdfb0603dd62804f8eecb79d99bf2976f19
James Kiger <james.kiger@gmail.com>
2025-06-18 15:09:20 -0400

    Add sanitize_text string filter for stripping potential urls from org names for email purposes

--- glitchtip/templatetags/settings.py
+++ glitchtip/templatetags/settings.py
@@ -1,5 +1,8 @@
+import re
+
 from django import template
 from django.conf import settings
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -7,3 +10,10 @@ register = template.Library()
 @register.simple_tag()
 def get_domain():
     return settings.GLITCHTIP_URL.geturl()
+
+
+@register.filter()
+@stringfilter
+def sanitize_text(string):
+    stripped_text = re.sub(r"\.com|http|\/|\.|\:|\$", "", string)
+    return stripped_text[:60]
